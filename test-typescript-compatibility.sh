13fef6f599a81707d2dfd4d6548a240e7c1d484b
Jacob Roberts <jacobdr@gmail.com>
2018-11-05 20:44:13 -0800

    Update ts version compatability script (#134)

--- test-typescript-compatibility.sh
+++ test-typescript-compatibility.sh
@@ -1,11 +1,31 @@
-#!/bin/sh
-version=$1
-temp_dir=$(mktemp -d)
-package_path=`pwd`
+#!/usr/bin/env bash
+set -euo pipefail
 
-cd $temp_dir && \
-yarn init -y && \
-yarn add typescript/@$version && \
-yarn add $package_path && \
-echo 'import * as hWs from "hydrated-ws/dist";' > test.ts && \
-./node_modules/.bin/tsc test.ts && cd $package_path || { cd $package_path ; exit 1; }
+version="${1:?Must supply a valid TS version}"
+package_path="$(pwd)"
+temp_dir="${package_path}/temp"
+temp_file="${temp_dir}/test.ts"
+rm -rf "${temp_dir}"
+mkdir "${temp_dir}"
+
+rm -rf ./dist && \
+echo "Installing typescript version: ${version}" && \
+yarn add -D "typescript/@${version}" && \
+yarn run tsc --noEmitOnError && \
+echo "Writing test file to: ${temp_file}" && \
+echo 'import * as hWs from "../dist";' > "${temp_file}" && \
+cd "${temp_dir}" && \
+echo "Starting compilatin of dummy project in $(pwd)" && \
+yarn run tsc --init && \
+yarn run tsc --noEmitOnError
+
+node "test.js"
+exit="${?}"
+
+echo "Node exit code was: ${exit}"
+
+if [[ "${exit}" != 0 ]]; then 
+    echo "Compilation failed for version ${version}"; exit 100;
+else
+    echo "Compilation succeeded for version: ${version}"
+fi
